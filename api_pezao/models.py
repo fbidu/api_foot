7e245f758908ef4c13b1fecf0c936d93d3297e8a
L Peres <luaspos@gmail.com>
2020-05-27 12:08:40 -0300

    initial modelling of db tables via sqlalchemy on models.py, firt sketch of modeling on shcema.py.

--- api_pezao/models.py
+++ api_pezao/models.py
@@ -1,21 +1,164 @@
 """
 Armazenar os modelos de dados para o banco
 """
-from sqlalchemy import Column, Integer, String
+import enum
+import datetime
+from sqlalchemy import Column, Integer, String, DateTime
 
 from .database import Base
 
 
 # pylint: disable=too-few-public-methods
+class HospitalType(enum.Enum):
+    H = 'H'
+    C = 'C'
+
 class User(Base):
     """
-    Defines the SQLAlchemy model for an user
+    Defines the SQLAlchemy model for 'user' table
     """
 
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
-    email = Column(String, unique=True, index=True)
     cpf = Column(String, unique=True, index=True)
-    first_name = Column(String)
-    last_name = Column(String)
+    email = Column(String, unique=True, index=True)
+    password = Column(String)
+    created_at = Column(DateTime, default=datetime.datetime.utcnow)
+    updated_at = Column(DateTime, default=datetime.datetime.utcnow)
+
+class Roles_Users(Base):
+    """
+    Defines the SQLAlchemy model for 'roles_users' table
+    """
+
+    __tablename__ = "roles_users"
+
+    user_id = Column(Integer, unique=True, index=True)
+    role_id = Column(Integer, unique=True, index=True)
+
+class Roles(Base):
+    """
+    Defines the SQLAlchemy model for 'roles' table
+    """
+
+    __tablename__ = "roles"
+
+    role_id = Column(Integer, primary_key=True, index=True)
+    role_name = Column(String, unique=True, index=True)
+
+class Hospital_Cs(Base):
+    """
+    Defines the SQLAlchemy model for 'hospital_cs' table
+    """
+
+    __tablename__ = "hospital_cs"
+
+    id = Column(Integer, primary_key=True, index=True)
+    code = Column(String, unique=True, index=True)
+    name = Column(String)
+    type = Column(enum.Enum(HospitalType))
+    email1 = Column(String)
+    email2 = Column(String)
+    email3 = Column(String)
+    user_id = Column(Integer)
+    created_at = Column(DateTime, default=datetime.datetime.utcnow)
+    updated_at = Column(DateTime, default=datetime.datetime.utcnow)
+
+
+class Log(Base):
+    """
+    Defines the SQLAlchemy model for 'log' table
+    """
+
+    __tablename__ = "log"
+
+    id = Column(Integer, primary_key=True, index=True)
+    results_id = Column(Integer, unique=True, index=True)
+    user_id = Column(Integer, unique=True, index=True)
+    client_ip = Column(String)
+    client_date_time = Column(DateTime, default=datetime.datetime.utcnow)
+    created_at = Column(DateTime, default=datetime.datetime.utcnow)
+    updated_at = Column(DateTime, default=datetime.datetime.utcnow)
+
+class Templates_Result(Base):
+    """
+    Defines the SQLAlchemy model for 'templates_result' table
+    """
+
+    __tablename__ = "templates_result"
+
+    result_id = Column(Integer, unique=True, index=True)
+    template_id = Column(Integer, unique=True, index=True)
+
+class Templates_SMS(Base):
+    """
+    Defines the SQLAlchemy model for 'templates_sms' table
+    """
+
+    __tablename__ = "templates_sms"
+
+    id = Column(Integer, primary_key=True, index=True)
+    msg = Column(String)
+
+class Attempt_Log(Base):
+    """
+    Defines the SQLAlchemy model for 'attempt_log' table
+    """
+
+    __tablename__ = "attempt_log"
+
+    id = Column(Integer, primary_key=True, index=True)
+    login = Column(String)
+    password = Column(String)
+    correct_code = Column(String)
+    code = Column(String)
+    cpf = Column(String)
+    dnv = Column(String)
+    dtnasc = Column(String)
+    client_ip = Column(String)
+    client_date_time = Column(DateTime, default=datetime.datetime.utcnow)
+    created_at = Column(DateTime, default=datetime.datetime.utcnow)
+    updated_at = Column(DateTime, default=datetime.datetime.utcnow)
+
+class Result(Base):
+    """
+    Defines the SQLAlchemy model for 'result' table
+    """
+
+    #some stuff here im unsure about, waiting for answers from the client, ana will ask her
+
+    __tablename__ = "result"
+
+    id = Column(Integer, primary_key=True, index=True)
+    IDExport = Column(Integer, unique=True, index=True)
+    Barcode = Column(Integer) #type?
+    NumLote = Column(Integer)
+    DataNasc = Column(String) #date?
+    HoraNasc = Column(String) #date?
+    DataColeta = Column(String) #date?
+    HoraColeta = Column(String) #date?
+    prMotherFirstname = Column(String)
+    prMotherSurname = Column(String)
+    CPF = Column(String)
+    ptnFirstname = Column(String)
+    ptnSurname = Column(String)
+    DNV = Column(String)
+    CNS = Column(String) #type?
+    ptnEmail = Column(String)
+    ptnPhone1 = Column(String)
+    ptnPhone2 = Column(String)
+    CodLocColeta = Column(String) #type?
+    LocalColeta = Column(String)
+    COD_LocColeta = Column(String) #type? repeat?
+    COD_HospNasc = Column(String) #type? repeat?
+    HospNasc = Column(String)
+    LocalNasc = Column(String)
+    PDF_Filename = Column(String)
+    Tipo_SMS = Column(String) #type?
+    RECODRD_CREATION_DATE = Column(DateTime, default=datetime.datetime.utcnow) #date?
+    FILE_EXPORT_DATE = Column(DateTime, default=datetime.datetime.utcnow) #date?
+    FILE_EXPORT_NAME = Column(String)
+    created_at = Column(DateTime, default=datetime.datetime.utcnow)
+    updated_at = Column(DateTime, default=datetime.datetime.utcnow)
+
--- api_pezao/schema.py
+++ api_pezao/schema.py
@@ -1,19 +1,25 @@
 """
 Define Pydantic models for the data structures.
 """
+
+import datetime
 from pydantic import BaseModel
 
 # pylint: disable=too-few-public-methods
+
+# a little confused on this whole part, did i do the bases right?
 class UserBase(BaseModel):
     """
     UserBase defines the fields available for an user in any
     point of its lifecycle.
     """
 
-    email: str
+    id: int
     cpf: str
-    first_name: str = None
-    last_name: str = None
+    email: str
+    password: str
+    created_at: datetime = None #datetime?
+    updated_at: datetime = None #datetime?
 
 
 class UserCreate(UserBase):
@@ -25,7 +31,336 @@ class UserCreate(UserBase):
     pass
 
 
-class User(UserBase):
+class User(UserBase): #i dont understand this bit :(
+    """
+    Defines a Pydantic model for an user
+    """
+
+    id: int
+
+    class Config:
+        """
+        Metadata to define the orm_mode of sqlalchemy
+        """
+
+        orm_mode = True
+
+
+#-----------------------------------------------
+
+class Roles_UsersBase(BaseModel):
+    """
+    Roles_UsersBase defines the fields available for an role_user in any
+    point of its lifecycle.
+    """
+    #descriptions are a bit wonky, im a little lost
+
+    user_id: int
+    role_id: int
+
+
+class Roles_UsersCreate(Roles_UsersBase):
+    """
+    UserCreate defines additional fields that should be used
+    while creating an user that weren't defined on `UserBase`
+    """
+
+    pass
+
+
+class Roles_Users(Roles_UsersBase):
+    """
+    Defines a Pydantic model for an user
+    """
+
+    id: int
+
+    class Config:
+        """
+        Metadata to define the orm_mode of sqlalchemy
+        """
+
+        orm_mode = True
+
+#-------------------------------------------------
+class RolesBase(BaseModel):
+    """
+    UserBase defines the fields available for an user in any
+    point of its lifecycle.
+    """
+
+    role_id: int
+    role_name: str
+
+
+class RolesCreate(RolesBase):
+    """
+    UserCreate defines additional fields that should be used
+    while creating an user that weren't defined on `UserBase`
+    """
+
+    pass
+
+
+class Roles(RolesBase):
+    """
+    Defines a Pydantic model for an user
+    """
+
+    id: int
+
+    class Config:
+        """
+        Metadata to define the orm_mode of sqlalchemy
+        """
+
+        orm_mode = True
+
+
+#-------------------------------------------------
+class Hospital_CsBase(BaseModel):
+    """
+    UserBase defines the fields available for an user in any
+    point of its lifecycle.
+    """
+
+    id: int
+    code: str
+    name: str
+    type: enumerate
+    email1: str
+    email2: str
+    email3: str
+    user_id: int
+    created_at: datetime #datetime?
+    updated_at: datetime #datetime?
+
+
+class Hospital_CsCreate(Hospital_CsBase):
+    """
+    UserCreate defines additional fields that should be used
+    while creating an user that weren't defined on `UserBase`
+    """
+
+    pass
+
+
+class Hospital_Cs(Hospital_CsBase):
+    """
+    Defines a Pydantic model for an user
+    """
+
+    id: int
+
+    class Config:
+        """
+        Metadata to define the orm_mode of sqlalchemy
+        """
+
+        orm_mode = True
+
+#-------------------------------------------------
+class LogBase(BaseModel):
+    """
+    UserBase defines the fields available for an user in any
+    point of its lifecycle.
+    """
+
+    id: int
+    results_id: int
+    user_id: int
+    client_ip: str
+    client_date_time: datetime #datetime?
+    created_at: datetime #datetime?
+    updated_at: datetime #datetime?
+
+
+class LogCreate(LogBase):
+    """
+    UserCreate defines additional fields that should be used
+    while creating an user that weren't defined on `UserBase`
+    """
+
+    pass
+
+
+class Log(LogBase):
+    """
+    Defines a Pydantic model for an user
+    """
+
+    id: int
+
+    class Config:
+        """
+        Metadata to define the orm_mode of sqlalchemy
+        """
+
+        orm_mode = True
+
+#-------------------------------------------------
+class Templates_ResultBase(BaseModel):
+    """
+    UserBase defines the fields available for an user in any
+    point of its lifecycle.
+    """
+
+    result_id: int
+    template_id: int
+
+
+class Templates_ResultCreate(Templates_ResultBase):
+    """
+    UserCreate defines additional fields that should be used
+    while creating an user that weren't defined on `UserBase`
+    """
+
+    pass
+
+
+class Templates_Result(Templates_ResultBase):
+    """
+    Defines a Pydantic model for an user
+    """
+
+    id: int
+
+    class Config:
+        """
+        Metadata to define the orm_mode of sqlalchemy
+        """
+
+        orm_mode = True
+
+#-------------------------------------------------
+class Templates_SMSBase(BaseModel):
+    """
+    UserBase defines the fields available for an user in any
+    point of its lifecycle.
+    """
+
+    id: int
+    msg: str
+
+
+class Templates_SMSCreate(Templates_SMSBase):
+    """
+    UserCreate defines additional fields that should be used
+    while creating an user that weren't defined on `UserBase`
+    """
+
+    pass
+
+
+class Templates_SMS(Templates_SMSBase):
+    """
+    Defines a Pydantic model for an user
+    """
+
+    id: int
+
+    class Config:
+        """
+        Metadata to define the orm_mode of sqlalchemy
+        """
+
+        orm_mode = True
+
+#-------------------------------------------------
+class Attempt_LogBase(BaseModel):
+    """
+    UserBase defines the fields available for an user in any
+    point of its lifecycle.
+    """
+
+    id: int
+    login: str
+    password: str
+    correct_code: str
+    code: str
+    cpf: str
+    dnv: str
+    dtnasc: str
+    client_ip: str
+    client_date_time: datetime #datetime?
+    created_at: datetime #datetime?
+    updated_at: datetime #datetime?
+
+
+class Attempt_LogCreate(Attempt_LogBase):
+    """
+    UserCreate defines additional fields that should be used
+    while creating an user that weren't defined on `UserBase`
+    """
+
+    pass
+
+
+class Attempt_Log(Attempt_LogBase):
+    """
+    Defines a Pydantic model for an user
+    """
+
+    id: int
+
+    class Config:
+        """
+        Metadata to define the orm_mode of sqlalchemy
+        """
+
+        orm_mode = True
+
+#-------------------------------------------------
+class ResultBase(BaseModel):
+    """
+    UserBase defines the fields available for an user in any
+    point of its lifecycle.
+    """
+
+
+    id: int
+    IDExport: int
+    Barcode: int #type?
+    NumLote: int #fieldname?
+    DataNasc: str #date?
+    HoraNasc: str #date?
+    DataColeta: str #date?
+    HoraColeta: str #date?
+    ptnMotherFirstname: str #fieldname?
+    ptnMotherSurname: str #fieldname?
+    CPF: str
+    ptnFirstname: str
+    ptnSurname: str
+    DNV: str
+    CNS: str #type?
+    ptnEmail: str
+    ptnPhone1: str
+    ptnPhone2: str
+    CodLocColeta: str #type?
+    LocalColeta: str
+    COD_LocColeta: str #type? repeat? fieldname?
+    COD_HospNasc: str #type? repeat? fieldname?
+    HospNasc: str
+    LocalNasc: str
+    PDF_Filename: str
+    Tipo_SMS: str #type?
+    RECODRD_CREATION_DATE: datetime #date?
+    FILE_EXPORT_DATE: datetime #date?
+    FILE_EXPORT_NAME: str
+    created_at: datetime #datetime?
+    updated_at: datetime #datetime?
+
+
+class ResultCreate(ResultBase):
+    """
+    UserCreate defines additional fields that should be used
+    while creating an user that weren't defined on `UserBase`
+    """
+
+    pass
+
+
+class Result(ResultBase):
     """
     Defines a Pydantic model for an user
     """
