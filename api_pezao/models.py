8f2d3b08a5d14efd17857a07773d180104d8bd4e
Luara Peres Oliveira da Silva <l172519@dac.unicamp.br>
2020-06-05 15:09:29 -0300

    Apply suggestion to api_pezao/models.py

--- api_pezao/models.py
+++ api_pezao/models.py
@@ -22,6 +22,7 @@ class User(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     cpf = Column(String, unique=True, index=True)
+    name = Column(String)
     email = Column(String, unique=True, index=True)
     password = Column(String)
     created_at = Column(DateTime, default=datetime.datetime.utcnow)
