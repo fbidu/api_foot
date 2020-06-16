7b368af002d09eb584e3983aef6530a296639aeb
Felipe Rodrigues <felipe@felipevr.com>
2020-06-16 10:52:09 -0300

    Extrai modelo de template-result

--- api_pezao/_models.py
+++ api_pezao/_models.py
@@ -3,20 +3,9 @@ Armazenar os modelos de dados para o banco
 """
 
 import datetime
-from sqlalchemy import Column, Integer, String, DateTime, PrimaryKeyConstraint
+from sqlalchemy import Column, Integer, String, DateTime
 
 from .database import Base
-
-
-class TemplatesResult(Base):
-    """
-    Defines the SQLAlchemy model for 'templates_result' table
-    """
-
-    __tablename__ = "templates_result"
-    __table_args__ = (PrimaryKeyConstraint("result_id", "template_id"),)
-    result_id = Column(Integer, index=True)
-    template_id = Column(Integer, index=True)
 
 
 class TemplatesSMS(Base):
--- api_pezao/models/__init__.py
+++ api_pezao/models/__init__.py
@@ -5,4 +5,5 @@ from .hospital_cs import HospitalCS, HospitalType
 from .log import Log
 from .roles import Roles
 from .roles_users import RolesUsers
+from .templates_result import TemplatesResult
 from .user import User
