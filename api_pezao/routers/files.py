ee700326f0222abd393072ba2676ebcb4968d1b9
Felipe Rodrigues <felipe@felipevr.com>
2020-07-31 17:53:16 -0300

    Merge

--- api_pezao/routers/files.py
+++ api_pezao/routers/files.py
@@ -1,14 +1,15 @@
 """
 Files router
 """
+
+from enum import Enum
 from pathlib import Path
 
 from fastapi import Depends, File, UploadFile, APIRouter, Header, HTTPException
 from sqlalchemy.orm import Session
 
 from .. import config, log
-
-from ..csv_input import import_results_csv
+from ..csv_input import import_results_csv, import_templates_results_csv
 
 from ..pdf_input import save_pdf
 from ..schemas.pdf_processed import PDFProcessed
@@ -18,8 +19,19 @@ from ..deps import get_db, get_settings
 router = APIRouter()
 
 
+class CSVTypes(str, Enum):
+    """
+    Tipos de CSV aceitos para importação
+    """
+
+    results = "results"
+    templates_results = "templates_results"
+    hospitals = "hospitals"
+
+
 @router.post("/csv/")
 def read_csv(
+    type: CSVTypes,
     csv_file: UploadFile = File(...),
     db: Session = Depends(get_db),
     authorization: str = Header(None),
@@ -31,11 +43,16 @@ def read_csv(
     if authorization != settings.upload_secret:
         raise HTTPException(401, "Operação inválida!")
 
+    lines = 0
+
     with csv_file.file as file:
         content = file.read()
         content = content.decode("utf-8")
         content = content.split("\n")
-        lines = len(import_results_csv(content, db))
+        if type == CSVTypes.results:
+            lines = len(import_results_csv(content, db))
+        elif type == CSVTypes.templates_results:
+            lines = len(import_templates_results_csv(content, db))
 
     log("[CSV] CSV foi importado.", db)
 
